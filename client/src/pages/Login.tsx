be6b0ac5a5f01f4f869817f016b9921474c74aaa
Joseph Waugh <josephwaugh312@github.com>
2025-04-27 17:24:10 -0400

    Fix duplicate GoogleIconSVG declaration in Login.tsx

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -21,27 +21,8 @@ import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { API_BASE_URL } from '../config';
 import GoogleIcon from '../assets/icons/google.svg';
 import GithubIcon from '../assets/icons/github.svg';
-import GoogleIconSVG from '../assets/icons/google.svg';
 import FacebookIcon from '../assets/icons/facebook.svg';
 import { useTheme } from '../context/ThemeContext';
-
-// SVG Icons for OAuth providers
-const GoogleIconSVG = () => (
-  <svg className="w-5 h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
-      <path fill="#4285F4" d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z" />
-      <path fill="#34A853" d="M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z" />
-      <path fill="#FBBC05" d="M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z" />
-      <path fill="#EA4335" d="M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z" />
-    </g>
-  </svg>
-);
-
-const FacebookIcon = () => (
-  <svg className="w-5 h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" fill="currentColor" />
-  </svg>
-);
 
 const Login: React.FC = () => {
   const [loginIdentifier, setLoginIdentifier] = useState('');
